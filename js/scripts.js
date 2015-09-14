c6e8d5da46a9d37e1c7b1d5c8818ce72e2a0768d
Jing Wait & Tim Kellogg <student@epicodus.com>
2015-09-14 11:39:05 -0700

    add ability to add landmarks

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
-
-  // get the inputs from the form
+  $("#add-landmark").click(function() {
+    $("#new-landmark").append(
+      '<div class="form-group">'+
+        '<label for="new-landmark">Landmark</label>'+
+        '<input type="text" required class="form-control" id="new-landmark">'+
+      '</div>'
+      );
+  });
 
   $('form').submit(function(event) {
     event.preventDefault();
-
     var inputtedName     = $("input#new-name").val();
     var inputtedLocation = $("input#new-location").val();
     var inputtedTime     = $("input#new-time").val();
     var inputtedNote     = $("input#new-note").val();
-
     var newPlace = { name: inputtedName, location: inputtedLocation, time: inputtedTime,
-                     note: inputtedNote }
+                     note: inputtedNote, landmarks: [] }
 
-    $("ul#places").append("<li><span class='place'>" + newPlace.name + ", " + newPlace.location +
-                          ", " + newPlace.time + ", " + newPlace.note + "</span></li>")
+    $(".new-landmark").each(function() {
+      var inputtedLandmark = $(this).find("input.new-landmark-name").val();
+      var newLandmark = { name: inputtedLandmark };
+      newPlace.landmarks.push(newLandmark);
+    });
 
+    $("ul#places").append("<li><span class='place'>" + newPlace.name + "</span></li>")
     $("input#new-name").val("");
     $("input#new-location").val("");
     $("input#new-time").val("");
     $("input#new-note").val("");
+    $("ul#landmarks").text("");
 
     $(".place").last().click(function() {
       $("#show-place").show();
@@ -28,7 +37,11 @@ $(document).ready(function() {
       $(".new-location").text(newPlace.location);
       $(".new-time").text(newPlace.time);
       $(".new-note").text(newPlace.note);
-      $(".new-landmark").text(newPlace.landmark);
+      newPlace.landmarks.forEach(function(landmark) {
+        console.log(landmark);
+        console.log(landmark.name);
+        $("ul#landmarks").append("<li>" + landmark.name + "</li>");
+      });
     });
   });
 });
